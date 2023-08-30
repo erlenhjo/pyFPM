6744f93cca335d78657cb33a5d977f46d2d28bc7
erlenhjo <103656986+erlenhjo@users.noreply.github.com>
2023-08-30 20:23:00 +0200

    more lens info

--- src/pyFPM/setup/components/lenses.py
+++ src/pyFPM/setup/components/lenses.py
@@ -5,13 +5,30 @@ class Lens_type(Enum):
     TELECENTRIC = 2
 
 class Lens(object):
-    def __init__(self, NA, magnification, infinity_corrected):
+    def __init__(
+            self, 
+            NA, 
+            magnification, 
+            diameter,
+            focal_length,
+            working_distance,
+            depth_of_field, 
+            lens_type: Lens_type
+        ):
         self.NA = NA
         self.magnification = magnification
-        self.infinity_corrected = infinity_corrected
+        self.diameter = diameter
+        self.focal_length = focal_length
+        self.working_distance = working_distance
+        self.depth_of_field = depth_of_field
+        self.lens_type: Lens_type = lens_type
 
 INFINITYCORRECTED_2X = Lens(
     NA=0.055,
     magnification = 2,
+    diameter = None,
+    focal_length = 100e-3,
+    working_distance = 34e-3,
+    depth_of_field = 91e-6,
     lens_type = Lens_type.INFINITY_CORRECTED
 )
