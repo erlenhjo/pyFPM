347678b65150e53a6687f17b530c93127b375e37
erlenhjo <103656986+erlenhjo@users.noreply.github.com>
2023-08-30 20:23:30 +0200

    rename camera variables

--- src/pyFPM/setup/components/cameras.py
+++ src/pyFPM/setup/components/cameras.py
@@ -1,11 +1,10 @@
 class Camera(object):
-    def __init__(self, ccd_pixel_size, image_size):
+    def __init__(self, ccd_pixel_size, raw_image_size):
         self.ccd_pixel_size = ccd_pixel_size #m
-        self.image_size = image_size # number of pixels in [x, y]
-        #bitdepth?
+        self.raw_image_size = raw_image_size # number of pixels in [x, y]
 
 
 HAMAMATSU = Camera(
     ccd_pixel_size = 6.5e-6,
-    image_size = [2048, 2048]
-)
+    raw_image_size = [2048, 2048]
+    )
