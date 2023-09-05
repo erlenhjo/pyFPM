1fe0972a395068b4246166d886bb1a376652b9a4
erlenhjo <103656986+erlenhjo@users.noreply.github.com>
2023-09-05 19:23:47 +0200

    ogaboga eureka

--- src/pyFPM/calibration/defocus_calibration.py
+++ src/pyFPM/calibration/defocus_calibration.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 from pyFPM.pre_processing.Preprocessed_data import Preprocessed_data
 from pyFPM.pre_processing.Illumination_pattern import Illumination_pattern
-from pyFPM.setup.Setup_parameters import Setup_parameters
 from pyFPM.setup.Imaging_system import Imaging_system
-from pyFPM.recovery.algorithms.Algorithm_result import Algorithm_result
+from pyFPM.recovery.utility.FPM_error import computeFPMerror
+
 
 from pyFPM.recovery.algorithms.primitive_FP import primitive_fourier_ptychography_algorithm
 
@@ -20,6 +20,8 @@ def primitive_defocus_calibration(
     loops = 10
     defocus_range = np.arange(-200,201,20) * 1e-6
 
+    errors = []
+
     for defocus in defocus_range:
         pupil = imaging_system.get_pupil(defocus = defocus)
 
@@ -31,19 +33,23 @@ def primitive_defocus_calibration(
             loops = loops
         )
         
-        weighted_obj = (np.mean(preprocessed_data.amplitude_images[illumination_pattern.update_order[0]]) \
-                       /  np.mean(abs(FP_results))) * FP_results   
+        sum_square_error = computeFPMerror(
+            preprocessed_data=preprocessed_data,
+            imaging_system=imaging_system,
+            illumination_pattern=illumination_pattern,
+            algorithm_result=FP_results
+        )
 
-        # % Simulate imaging with weighted object
-        # simu_im_seq = recovery.utility.get_im_seq(weighted_obj, im_sys, defocus);
+        errors.append(sum_square_error)
 
-        # sse_per_angle(:) = sum(sum((abs(simu_im_seq)-amplitude_im_seq).^2));
-        # % remove the ones ignored from ignore_list
-        # ss_error(nr) = sum(sse_per_angle .*(1-ignore_list));
+
+    plt.scatter(defocus_range*1e6,errors)
+    plt.title("Sum square error per defocus")
+    plt.xlabel("Defocus [µm]")
+    plt.ylabel("SSE [a.u.]")
+    plt.tight_layout()
+    plt.show()
 
 
 
         
-        plt.matshow(np.angle(FP_results))
-        plt.title(f"defocus = {defocus*1e6:.3f} um")
-    plt.show()
---
+++ src/pyFPM/recovery/utility/FPM_error.py
@@ -0,0 +1,60 @@
+import numpy as np
+from numpy.fft import ifft2, ifftshift, fft2, fftshift
+
+from pyFPM.pre_processing.Preprocessed_data import Preprocessed_data
+from pyFPM.pre_processing.Illumination_pattern import Illumination_pattern
+from pyFPM.setup.Imaging_system import Imaging_system
+from pyFPM.recovery.algorithms.Algorithm_result import Algorithm_result
+
+def computeFPMerror(preprocessed_data: Preprocessed_data, imaging_system: Imaging_system, 
+                            illumination_pattern: Illumination_pattern, algorithm_result: Algorithm_result):
+    
+    low_res_images = preprocessed_data.amplitude_images
+    recovered_object = algorithm_result.recovered_object
+    pupil = algorithm_result.pupil
+
+    update_order = illumination_pattern.update_order  # order does not matter, but this is a list only of "active" LEDs
+    size_low_res_x = imaging_system.patch_size[0]
+    size_low_res_y = imaging_system.patch_size[1]
+    size_high_res_x = imaging_system.final_image_size[0]
+    size_high_res_y = imaging_system.final_image_size[1]
+    k_x = imaging_system.wavevectors_x()
+    k_y = imaging_system.wavevectors_y()
+    dk_x = imaging_system.differential_wavevectors_x()
+    dk_y = imaging_system.differential_wavevectors_y()
+    low_res_CTF = imaging_system.low_res_CTF
+    inverse_scaling_factor = 1 / imaging_system.pixel_scale_factor
+    LED_indices = preprocessed_data.LED_indices
+
+    weighted_object_ft = np.mean(low_res_images[update_order[0]])\
+                    / np.mean((np.abs(recovered_object))) * recovered_object
+    
+    recovered_object_ft = fftshift(fft2(fftshift(weighted_object_ft)))
+
+    error_estimate = 0
+    e1=0
+    e2=0
+    for image_nr in range(len(update_order)):
+        index = update_order[image_nr]
+        LED_index_x = LED_indices[index][0]
+        LED_index_y = LED_indices[index][1]
+
+        raw_low_res_image = low_res_images[index]
+
+        # calculate which wavevector-values are present in the current low res image
+        k_center_x = round((size_high_res_x - 1)/2 - k_x[LED_index_y, LED_index_x]/dk_x)
+        k_center_y = round((size_high_res_y - 1)/2 - k_y[LED_index_y, LED_index_x]/dk_y)
+        k_min_x = int(np.floor(k_center_x - (size_low_res_x - 1) / 2))
+        k_max_x = int(np.floor(k_center_x + (size_low_res_x - 1) / 2))
+        k_min_y = int(np.floor(k_center_y - (size_low_res_y - 1) / 2))
+        k_max_y = int(np.floor(k_center_y + (size_low_res_y - 1) / 2))
+
+
+        recovered_low_res_ft = inverse_scaling_factor**2 * recovered_object_ft[k_min_y:k_max_y+1, k_min_x:k_max_x+1] \
+                                * low_res_CTF * pupil
+        recovered_low_res_image =  np.abs(ifftshift(ifft2(ifftshift(recovered_low_res_ft))))
+        
+        error_estimate += np.sum((raw_low_res_image - recovered_low_res_image)**2)
+        
+    return error_estimate
+
