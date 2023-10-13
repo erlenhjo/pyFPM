d475b23e41740bed3502bb02593a97fc077b2692
erlenhjo <103656986+erlenhjo@users.noreply.github.com>
2023-10-13 12:50:33 +0200

    profiling test

--- examples/simulate_and_recover.py
+++ examples/simulate_and_recover.py
@@ -3,31 +3,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # FPM imports
-from pyFPM.NTNU_specific.simulated_images import simulate_cameraman_2x
+from pyFPM.NTNU_specific.simulations.simulated_images import simulate_cameraman_2x
 from pyFPM.recovery.algorithms.run_algorithm import recover, Method
 
 # import plotting
 from plotting.plot_simulation_results import plot_simulation_results
 
-method = Method.Epry_Gradient_Descent
-loops = 100
 
-setup_parameters, data_patch, imaging_system, illumination_pattern, applied_pupil\
-      = simulate_cameraman_2x(noise_fraction=0, zernike_coefficients=[0,0,0,0,0.1])
+def main():
+    method = Method.Epry_Gradient_Descent
+    loops = 100
+
+    setup_parameters, data_patch, imaging_system, illumination_pattern, applied_pupil\
+        = simulate_cameraman_2x(noise_fraction=0, zernike_coefficients=[0,0,0,0,0.1])
 
 
-# define pupil
-pupil_guess = applied_pupil * 0 + 1
+    # define pupil
+    pupil_guess = applied_pupil * 0 + 1
 
-algorithm_result = recover(
-    method=method,
-    data_patch=data_patch,
-    imaging_system=imaging_system,
-    illumination_pattern=illumination_pattern,
-    pupil_guess=pupil_guess,
-    loops=loops
-)
+    algorithm_result = recover(
+        method=method,
+        data_patch=data_patch,
+        imaging_system=imaging_system,
+        illumination_pattern=illumination_pattern,
+        pupil_guess=pupil_guess,
+        loops=loops
+    )
 
-plot_simulation_results(data_patch, illumination_pattern, imaging_system, algorithm_result)
+    plot_simulation_results(data_patch, illumination_pattern, imaging_system, algorithm_result)
 
-print(algorithm_result.convergence_index)
+    print(algorithm_result.convergence_index)
+
+
+def profile_main():
+    import cProfile
+    import pstats
+
+    with cProfile.Profile() as pr:
+        main()
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.dump_stats(filename=r"profiling_data\simulate_and_recover_jitted_fftshifts.prof")
+
+if __name__ == "__main__":
+    #profile_main()
+    main()
