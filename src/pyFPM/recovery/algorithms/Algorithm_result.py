92df7488e9f2d1e10e89f696e75705542a458ed4
erlenhjo <103656986+erlenhjo@users.noreply.github.com>
2023-10-18 19:03:47 +0200

    real space error metric

--- src/pyFPM/recovery/algorithms/Algorithm_result.py
+++ src/pyFPM/recovery/algorithms/Algorithm_result.py
@@ -1,6 +1,10 @@
-class Algorithm_result(object):
-    def __init__(self, recovered_object, recovered_object_fourier_transform, pupil, convergence_index):
-        self.recovered_object = recovered_object
-        self.recovered_object_fourier_transform = recovered_object_fourier_transform
-        self.pupil = pupil
-        self.convergence_index = convergence_index
+from dataclasses import dataclass
+import numpy as np
+
+@dataclass
+class Algorithm_result:
+    recovered_object: np.ndarray
+    recovered_object_fourier_transform: np.ndarray
+    pupil: np.ndarray
+    convergence_index: np.ndarray
+    real_space_error_metric: np.ndarray
--- src/pyFPM/recovery/algorithms/fraunhofer_algorithm.py
+++ src/pyFPM/recovery/algorithms/fraunhofer_algorithm.py
@@ -8,7 +8,6 @@ from pyFPM.setup.Imaging_system import Imaging_system
 from pyFPM.recovery.algorithms.Algorithm_result import Algorithm_result
 from pyFPM.recovery.utility.k_space import calculate_k_vector_range, calculate_recovered_CTF
 from pyFPM.recovery.algorithms.initialization import initialize_high_res_image, extract_variables
-#from pyFPM.numba_helper.jitable_fftshifts import fftshift, ifftshift
 
 def fraunhofer_recovery_algorithm(
         data_patch: Data_patch,
@@ -27,7 +26,7 @@ def fraunhofer_recovery_algorithm(
     recovered_object_guess = initialize_high_res_image(low_res_images, update_order, scaling_factor)
     
 
-    recovered_object_fourier_transform, pupil, convergence_index \
+    recovered_object_fourier_transform, pupil, convergence_index, real_space_error_metric \
         = main_algorithm_loop(recovered_object_guess, loops, use_epry, use_gradient_descent, update_order, low_res_images,  LED_indices,
                         k_x, k_y, dk_x, dk_y, size_low_res_x, size_low_res_y, size_high_res_x, size_high_res_y, scaling_factor_squared, low_res_CTF, pupil
                         )
@@ -39,7 +38,8 @@ def fraunhofer_recovery_algorithm(
         recovered_object = fftshift(ifft2(ifftshift(recovered_object_fourier_transform))),
         recovered_object_fourier_transform = recovered_object_fourier_transform,
         pupil = pupil,
-        convergence_index = convergence_index
+        convergence_index = convergence_index,
+        real_space_error_metric = real_space_error_metric
     ) 
     return algorithm_result
 
@@ -50,9 +50,12 @@ def main_algorithm_loop(recovered_object_guess, loops, use_epry, use_gradient_de
                         ):
     recovered_object_fourier_transform = fftshift(fft2(ifftshift(recovered_object_guess)))  
     convergence_index = np.zeros(loops)
+    real_space_error_metric = np.zeros(loops)
     low_res_images = low_res_images * scaling_factor_squared
 
     for loop_nr in range(loops):
+        error_metric_numerator = 0
+        error_metric_denominator = 0
         for image_nr in range(len(update_order)):
             index = update_order[image_nr]
             
@@ -69,6 +72,9 @@ def main_algorithm_loop(recovered_object_guess, loops, use_epry, use_gradient_de
 
             convergence_index[loop_nr] += np.mean(np.abs(recovered_low_res_image)) \
                                             / np.sum(np.abs(np.abs(recovered_low_res_image) - raw_low_res_image))
+            
+            error_metric_numerator += np.linalg.norm(raw_low_res_image - np.abs(recovered_low_res_image))**2
+            error_metric_denominator += np.linalg.norm(raw_low_res_image)**2
 
             new_recovered_low_res_image = raw_low_res_image * recovered_low_res_image / np.abs(recovered_low_res_image)
             
@@ -96,8 +102,11 @@ def main_algorithm_loop(recovered_object_guess, loops, use_epry, use_gradient_de
                 recovered_object_fourier_transform[k_min_y:k_max_y+1, k_min_x:k_max_x+1] += object_update_term * low_res_CTF
                 if loop_nr > 1:
                     pupil = (pupil + pupil_update_term) * low_res_CTF
+        
+        real_space_error_metric[loop_nr] = error_metric_numerator/error_metric_denominator
 
-    return recovered_object_fourier_transform, pupil, convergence_index
+
+    return recovered_object_fourier_transform, pupil, convergence_index, real_space_error_metric
 
 @njit(cache=True)
 def standard_step(phi, new_FT, old_FT):
