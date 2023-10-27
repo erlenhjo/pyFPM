8ce4e0aeb5db2918c15947d1f89801a03a180ea7
erlenhjo <103656986+erlenhjo@users.noreply.github.com>
2023-10-27 17:09:03 +0200

    Update k_space.py

--- src/pyFPM/recovery/utility/k_space.py
+++ src/pyFPM/recovery/utility/k_space.py
@@ -9,12 +9,12 @@ def calculate_k_vector_range(k_x, k_y, dk_x, dk_y,
     LED_index_y = LED_indices[index][1]
 
     # calculate which wavevector-values are present in the current low res image
-    k_center_x = np.round((size_high_res_x - 1)/2 - k_x[LED_index_y, LED_index_x]/dk_x)
-    k_center_y = np.round((size_high_res_y - 1)/2 - k_y[LED_index_y, LED_index_x]/dk_y)
-    k_min_x = int(np.floor(k_center_x - (size_low_res_x - 1) / 2))
-    k_max_x = int(np.floor(k_center_x + (size_low_res_x - 1) / 2))
-    k_min_y = int(np.floor(k_center_y - (size_low_res_y - 1) / 2))
-    k_max_y = int(np.floor(k_center_y + (size_low_res_y - 1) / 2))
+    k_center_x = (size_high_res_x - 1)/2 - k_x[LED_index_y, LED_index_x]/dk_x
+    k_center_y = (size_high_res_y - 1)/2 - k_y[LED_index_y, LED_index_x]/dk_y
+    k_min_x = int(np.round(k_center_x - (size_low_res_x - 1) / 2))
+    k_max_x = int(np.round(k_center_x + (size_low_res_x - 1) / 2))
+    k_min_y = int(np.round(k_center_y - (size_low_res_y - 1) / 2))
+    k_max_y = int(np.round(k_center_y + (size_low_res_y - 1) / 2))
 
     return k_min_x, k_max_x, k_min_y, k_max_y
 
