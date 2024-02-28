35a0b0100696a2d9d4355537fea74d2a6fde47f8
erlenhjo <103656986+erlenhjo@users.noreply.github.com>
2024-02-28 13:32:03 +0100

    improve initialization of object

--- src/pyFPM/recovery/algorithms/initialization.py
+++ src/pyFPM/recovery/algorithms/initialization.py
@@ -6,14 +6,20 @@ from pyFPM.recovery.algorithms.Step_description import Step_description
 
 import numpy as np
 from scipy.ndimage import zoom
+from numpy.fft import fftshift, ifftshift, fft2, ifft2
 
-def initialize_high_res_image(low_res_images, update_order, scaling_factor, object_phase_correction):
+def initialize_high_res_image(low_res_images, update_order, scaling_factor, 
+                              object_phase_correction, high_res_CTF):
     first_image = low_res_images[update_order[0]]
-    
-    recovered_object_guess = zoom(input=first_image, zoom=scaling_factor) * object_phase_correction
-    # recovered_object_guess = np.ones(shape=recovered_object_guess.shape) * object_phase_correction
+    #old
+    #recovered_object_guess = zoom(input=first_image, zoom=scaling_factor)*object_phase_correction
+    #new
+    recovered_object_guess = zoom(input=first_image, zoom=scaling_factor)
+    recovered_object_guess = ifft2(ifftshift(fftshift(fft2(recovered_object_guess)) * high_res_CTF))
+    recovered_object_spectrum_guess = ifftshift(fft2(fftshift(recovered_object_guess \
+                                                              * object_phase_correction)))
 
-    return recovered_object_guess
+    return recovered_object_guess, recovered_object_spectrum_guess
 
 
 def extract_variables(data_patch: Data_patch, 
@@ -36,6 +42,7 @@ def extract_variables(data_patch: Data_patch,
         shifts_y = imaging_system.LED_shifts_y_aperture
 
     low_res_CTF = imaging_system.low_res_CTF
+    high_res_CTF = imaging_system.high_res_CTF
     scaling_factor_squared = (imaging_system.pixel_scale_factor)**2
     scaling_factor = imaging_system.pixel_scale_factor
     LED_indices = data_patch.LED_indices
@@ -48,8 +55,8 @@ def extract_variables(data_patch: Data_patch,
     max_iterations = step_description.max_iterations
 
     return low_res_images, update_order, size_low_res_x, size_low_res_y, size_high_res_x, size_high_res_y, shifts_x, shifts_y,\
-            low_res_CTF, scaling_factor_squared, scaling_factor, LED_indices, alpha, beta, eta, start_EPRY_at_iteration,\
-            start_adaptive_steps_at_iteration, converged_alpha, max_iterations
+            low_res_CTF, high_res_CTF, scaling_factor_squared, scaling_factor, LED_indices, alpha, beta, eta,\
+            start_EPRY_at_iteration, start_adaptive_steps_at_iteration, converged_alpha, max_iterations
 
 
 def extract_variables_simulated_anealing(data_patch: Data_patch,
