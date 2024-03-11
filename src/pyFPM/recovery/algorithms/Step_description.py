86877c3e9d1464ae34877dbab48cb26ba1a7d700
erlenhjo <103656986+erlenhjo@users.noreply.github.com>
2024-03-11 16:56:28 +0100

    redefine standard adaptive step

--- src/pyFPM/recovery/algorithms/Step_description.py
+++ src/pyFPM/recovery/algorithms/Step_description.py
@@ -1,7 +1,4 @@
 from dataclasses import dataclass
-import numpy as np
-
-from pyFPM.setup.Illumination_pattern import Illumination_pattern
 
 @dataclass
 class Step_description:
@@ -14,8 +11,7 @@ class Step_description:
     start_adaptive_at_iteration: int
 
 
-def get_standard_adaptive_step_description(illumination_pattern: Illumination_pattern,
-                                           max_iterations: int,
+def get_standard_adaptive_step_description(max_iterations: int,
                                            start_EPRY_at_iteration: int,
                                            start_adaptive_at_iteration: int):
     return Step_description(alpha=1,
@@ -25,12 +21,3 @@ def get_standard_adaptive_step_description(illumination_pattern: Illumination_pa
                             max_iterations=max_iterations,
                             start_EPRY_at_iteration = start_EPRY_at_iteration,
                             start_adaptive_at_iteration = start_adaptive_at_iteration)
-
-def get_constant_step_description(max_iterations, start_EPRY_at_iteration):
-    return Step_description(alpha=1,
-                            beta=1,
-                            eta=None,
-                            converged_alpha=0,
-                            max_iterations=max_iterations,
-                            start_EPRY_at_iteration = start_EPRY_at_iteration,
-                            start_adaptive_at_iteration=2*max_iterations)
