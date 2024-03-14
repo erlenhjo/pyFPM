dbc2dd9405635fca7d03f6159c8e6d27331eec5a
erlenhjo <103656986+erlenhjo@users.noreply.github.com>
2024-03-14 15:46:54 +0100

    plot BF edge

---
+++ scripting/tests/bright_field.py
@@ -0,0 +1,129 @@
+from pyFPM.experimental.plot_illumination import plot_bright_field_images_with_BF_edge
+from pyFPM.NTNU_specific.simulate_images.only_illumination import simulate_illumination
+from pyFPM.NTNU_specific.setup_from_file import setup_parameters_from_file
+from pyFPM.NTNU_specific.rawdata_from_files import get_rawdata_from_files
+from pyFPM.NTNU_specific.components import HAMAMATSU_C11440_42U30, MAIN_LED_ARRAY, INFINITYCORRECTED_2X, COMPACT_2X, TELECENTRIC_3X, DOUBLE_CONVEX
+from pyFPM.setup.Imaging_system import LED_calibration_parameters
+from pyFPM.setup.Setup_parameters import Setup_parameters
+from pyFPM.setup.Data import Data_patch, Rawdata, Preprocessed_data
+
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+main_folder = Path.cwd() / "data"/ "Illumination" 
+
+def main():
+    simulation = False
+    real = True
+
+    infcor_2x(simulation, real)
+    telecentric_3x(simulation, real)
+    double_convex(simulation, real)
+    compact_2x(simulation, real)
+
+    plt.show()
+    
+def infcor_2x(simulation, real):
+    lens = INFINITYCORRECTED_2X
+    datadirpath = main_folder / "infinity_2x_illumnation"
+    calibration_parameters = LED_calibration_parameters(200e-3,0e-6,0e-6,0)
+    if simulation:
+        illustrate_illumination_from_simulation(lens=lens, spherical=True, Fresnel=False, arraysize=5,
+                                                calibration_parameters=calibration_parameters)
+    if real:
+        illustrate_illumination_from_setup(datadirpath=datadirpath, lens=lens, Fresnel=False,
+                                        array_size=5, calibration_parameters=calibration_parameters)
+    
+def compact_2x(simulation, real):
+    lens = COMPACT_2X
+    datadirpath = main_folder / "compact_2x_illumination"
+    calibration_parameters = LED_calibration_parameters(200e-3,0e-6,0e-6,0)
+    if simulation:
+        illustrate_illumination_from_simulation(lens=lens, spherical=True, Fresnel=True, arraysize=5,
+                                                calibration_parameters=calibration_parameters)
+    if real:
+        illustrate_illumination_from_setup(datadirpath=datadirpath, lens=lens, Fresnel=True,
+                                        array_size=5, calibration_parameters=calibration_parameters)
+    
+def telecentric_3x(simulation, real):
+    lens = TELECENTRIC_3X
+    datadirpath = main_folder / "telecentric_3x_illumination_2"
+    calibration_parameters = LED_calibration_parameters(200e-3,0e-6,0e-6,0)
+    if simulation:
+        illustrate_illumination_from_simulation(lens=lens, spherical=True, Fresnel=False, arraysize=5,
+                                                calibration_parameters=calibration_parameters)
+    if real:
+        illustrate_illumination_from_setup(datadirpath=datadirpath, lens=lens, Fresnel=False,
+                                        array_size=5, calibration_parameters=calibration_parameters)
+    
+def double_convex(simulation, real):
+    lens = DOUBLE_CONVEX
+    datadirpath = main_folder / "double_convex_illumination"
+    calibration_parameters = LED_calibration_parameters(200e-3,0e-6,0e-6,0)
+    if simulation:
+        illustrate_illumination_from_simulation(lens=lens, spherical=True, Fresnel=True, arraysize=5,
+                                                calibration_parameters=calibration_parameters)
+    if real:
+        illustrate_illumination_from_setup(datadirpath=datadirpath, lens=lens, Fresnel=True,
+                                        array_size=5, calibration_parameters=calibration_parameters)
+
+
+def illustrate_illumination_from_simulation(lens, spherical, Fresnel, arraysize, calibration_parameters):
+    setup_parameters, data_patch, imaging_system, illumination_pattern, applied_pupil, high_res_complex_object\
+       = simulate_illumination(lens = lens, 
+                               correct_spherical_wave_illumination = spherical, 
+                               correct_Fresnel_propagation = Fresnel,
+                               arraysize=arraysize,
+                               calibration_parameters=calibration_parameters,
+                               patch_offset=[0,0])
+
+    fig = plot_bright_field_images_with_BF_edge(data_patch=data_patch, setup_parameters=setup_parameters, 
+                                                calibration_parameters=calibration_parameters,
+                                                array_size=arraysize, Fresnel=Fresnel)
+
+    return fig
+
+def illustrate_illumination_from_setup(datadirpath, lens, Fresnel, array_size, calibration_parameters):
+    patch_offset = [0, 0] # [x, y]
+    patch_size = [2048, 2048] # [x, y]
+
+    camera = HAMAMATSU_C11440_42U30
+    LED_array = MAIN_LED_ARRAY 
+
+    setup_parameters: Setup_parameters = setup_parameters_from_file(
+        datadirpath = datadirpath,
+        lens = lens,
+        camera = camera,
+        LED_array = LED_array
+        )
+
+    rawdata: Rawdata = get_rawdata_from_files(
+        datadirpath = datadirpath,
+        image_format = setup_parameters.image_format,
+        center_indices = setup_parameters.LED_info.center_indices,
+        max_array_size = array_size
+        )
+
+    preprocessed_data = Preprocessed_data(
+        rawdata = rawdata,
+        setup_parameters = setup_parameters,
+        noise_reduction_regions = None, 
+        threshold_value = False
+        )
+    
+    data_patch = Data_patch(
+        data = preprocessed_data,
+        raw_image_size = setup_parameters.camera.raw_image_size,
+        patch_offset = patch_offset,
+        patch_size = patch_size
+        )
+
+    fig = plot_bright_field_images_with_BF_edge(data_patch=data_patch, setup_parameters=setup_parameters, 
+                                                calibration_parameters=calibration_parameters, 
+                                                array_size=array_size, Fresnel=Fresnel)
+    return fig
+
+    
+if __name__ == "__main__":
+    main()
+    
--- src/pyFPM/experimental/plot_illumination.py
+++ src/pyFPM/experimental/plot_illumination.py
@@ -34,3 +34,70 @@ def plot_bright_field_images(data_patch: Data_patch, setup_parameters: Setup_par
     
     return fig
 
+
+def plot_bright_field_images_with_BF_edge(data_patch: Data_patch, setup_parameters: Setup_parameters, 
+                                          calibration_parameters: LED_calibration_parameters, 
+                                          array_size: int, Fresnel: bool):
+    LED_indices = data_patch.LED_indices
+
+    center_indices = setup_parameters.LED_info.center_indices
+    center_x = center_indices[0]
+    center_y = center_indices[1]
+    y_min = center_y - array_size//2
+    x_min = center_x - array_size//2
+    y_max = y_min + array_size
+    x_max = x_min + array_size
+
+    max_intensity = np.max(data_patch.amplitude_images)**2
+
+    fig, axes = plt.subplots(nrows=array_size, ncols=array_size, figsize=(7,7), constrained_layout = True)
+
+    mean_values = np.empty(shape=(array_size,array_size))
+
+    for image_nr, indices in enumerate(LED_indices):
+        x,y = indices
+        if x>=x_min and x<x_max and y>=y_min and y<y_max:
+            m = y_max-y-1
+            n = x_max-x-1
+            LED_n = x - center_x
+            LED_m = y - center_y
+
+            axes[m,n].matshow(data_patch.amplitude_images[image_nr]**2, vmin=0, vmax=max_intensity)
+            axes[m,n].axis("off")
+            mean_values[m,n] = np.mean(data_patch.amplitude_images[image_nr]**2)
+
+            center, radius = calculate_BF_edge(setup_parameters=setup_parameters,
+                                               calibration_parameters=calibration_parameters,
+                                               LED_n=LED_n, LED_m=LED_m, Fresnel=Fresnel)
+            circle = plt.Circle(center, radius, fill=False, color="r", linestyle="dashed")
+            axes[m,n].add_patch(circle)
+            
+    return fig
+
+def calculate_BF_edge(setup_parameters: Setup_parameters, calibration_parameters: LED_calibration_parameters,
+                      LED_n, LED_m, Fresnel):
+    pixel_size = setup_parameters.camera.camera_pixel_size/setup_parameters.lens.magnification
+    LED_pitch = setup_parameters.LED_info.LED_pitch
+    image_center_x = setup_parameters.camera.raw_image_size[0] // 2
+    image_center_y = setup_parameters.camera.raw_image_size[1] // 2
+    z_0 = calibration_parameters.LED_distance
+    delta_x = calibration_parameters.LED_x_offset
+    delta_y = calibration_parameters.LED_y_offset
+    rotation = calibration_parameters.LED_rotation
+    NA = setup_parameters.lens.NA
+    focal_length = setup_parameters.lens.focal_length
+    magnification = setup_parameters.lens.magnification
+
+    if not Fresnel:
+        z_1 = np.inf
+    else:
+        z_1 = focal_length*(1+1/magnification)
+
+    radius = NA / (1/z_0+1/z_1) / pixel_size
+    center_x = (LED_pitch*LED_n*np.cos(rotation) - LED_pitch*LED_m*np.sin(rotation) + delta_x)\
+                /pixel_size / (1+z_0/z_1)
+    center_y = (LED_pitch*LED_n*np.sin(rotation) + LED_pitch*LED_m*np.cos(rotation) + delta_y)\
+                /pixel_size / (1+z_0/z_1)
+
+    
+    return (center_x + image_center_x, center_y + image_center_y), radius
